c61a50aefbc19b336fabd45fb0691c87999929c1
francky <contact@francky.works>
2023-08-29 22:26:30 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+import PrimarySearchAppBar from './components/common/PrimarySearchAppBar'
+
+function App() {
+
+  return (
+    <>
+      <PrimarySearchAppBar />
+    </>
+  )
+}
+
+export default App
