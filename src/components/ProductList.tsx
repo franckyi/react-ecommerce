831a5df59558fbfa3dc90c21e9202c47789a81e4
francky <contact@francky.works>
2023-09-07 04:40:12 +0200

    minor fixes

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,13 +4,11 @@ import Spinner from './ui/Spinner';
 
 export default function ProductList({ products, minRating, loading }: ProductListProps) {
     return (
-        <>
-            <section className="product-list">
-                {loading && <Spinner />}
-                {products && products.filter(item => item.rating.rate >= minRating).map(item => {
-                    return <ProductCard key={item.id} item={item} />
-                })}
-            </section>
-        </>
+        <section className="product-list">
+            {loading && <Spinner />}
+            {products && products.filter(item => item.rating.rate >= minRating).map(item => {
+                return <ProductCard key={item.id} item={item} />
+            })}
+        </section>
     )
 }
